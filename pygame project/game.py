9d45c495bad7fd5fedacb642575413ce347193d4
Actually IAmPlayer <iamplayergamingyt@gmail.com>
2023-04-29 16:01:00 +0700

    ladder almost works

--- pygame project/game.py
+++ pygame project/game.py
@@ -13,23 +13,28 @@ DIR_UP = -2
 DIR_DOWN = 2
 
 BLOCK_SOLID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-BLOCK_CLIMBABLE = [11]
+BLOCK_SEMISOLID = [12, 13]
+BLOCK_CLIMBABLE = [11, 12]
 
-# functions
-def getBlock(ids: list):
-    b = blocks
-    
-    #filter
-    if ids != []:
-        for k, v in enumerate(b):
-            if not(v.id in ids):
-                b.remove(v)
-    
-    return b
+WEAPON_TRIPLE_SHOT = 0
+WEAPON_CRYSTAL_BLAST = 1
+WEAPON_SHOCK_FORCE = 2
+WEAPON_SPIRAL_CYCLONE = 3
+WEAPON_BURNER_WAVE = 4
+WEAPON_LEAF_GUARD = 5
+WEAPON_DOWNPOUR_STORM = 6
+
+# used for boundaries in camera or npc behavior
+class Section():
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
+        self.x          = x
+        self.y          = y
+        self.width      = width
+        self.height     = height
 
 # classes
 class Player():
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: int, y: int, section: int) -> None:
         self.gfx            = pygame.image.load("pygame project\\test.png")
         self.rect           = self.gfx.get_rect()
         # Gameplay vars
@@ -38,42 +43,71 @@ class Player():
         self.speedX         = 0
         self.speedY         = 0
         self.direction      = 1
+        self.section        = section
+        self.maxHealth      = 16
+        self.health         = self.maxHealth
+        self.weapon         = 0
         # Starting location
-        self.rect.x         = x
-        self.rect.y         = y
+        self.rect.x         = sections[section].x + x
+        self.rect.y         = sections[section].y + y
         # Player states
         self.jumpTimer      = 0
         self.hasJumped      = 0
         self.attackCooldown = 0
         self.bulletsOut     = 0
         self.isOnGround     = True
+        self.isClimbing     = False
+        self.nearLadder     = False
     
-    def update(self):
-        if not camera.isUpdating:
-            if pygame.key.get_pressed():
-                if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                    self.direction = DIR_RIGHT
-                if pygame.key.get_pressed()[pygame.K_LEFT]:
-                    self.direction = DIR_LEFT
+    def handleControls(self):
+        if pygame.key.get_pressed():
+            if pygame.key.get_pressed()[pygame.K_RIGHT]:
+                self.direction = DIR_RIGHT
+            if pygame.key.get_pressed()[pygame.K_LEFT]:
+                self.direction = DIR_LEFT
+            
+            if not self.isClimbing:                    
                 self.speedX = self.direction
 
-                if (not pygame.key.get_pressed()[pygame.K_RIGHT]) and (not pygame.key.get_pressed()[pygame.K_LEFT]):
-                    self.speedX = 0
-                
                 if pygame.key.get_pressed()[pygame.K_z] and (not self.hasJumped) and self.isOnGround:
                     self.hasJumped = True
                 
-                if pygame.key.get_pressed()[pygame.K_s] and self.attackCooldown == 0 and self.bulletsOut < 3:
-                    self.attackCooldown = 10
-                    self.bulletsOut += 1
+                if (pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_DOWN]) and self.nearLadder:
+                    self.isClimbing = True
+            else:
+                if pygame.key.get_pressed()[pygame.K_UP]:
+                    self.speedY = -1
+                if pygame.key.get_pressed()[pygame.K_DOWN]:
+                    self.speedY = 1
 
-                    offset = 0
-                    if self.direction == 1:
-                        offset = self.width
+                if (not pygame.key.get_pressed()[pygame.K_UP]) and (not pygame.key.get_pressed()[pygame.K_DOWN]):
+                    self.speedY = 0
+                
+                # dismount from ladder
+                if pygame.key.get_pressed()[pygame.K_z]:
+                    self.isClimbing = False
 
-                    npcClass = NPC(1, self.rect.x + offset, self.rect.y, self.direction)
+            if (not pygame.key.get_pressed()[pygame.K_RIGHT]) and (not pygame.key.get_pressed()[pygame.K_LEFT]):
+                self.speedX = 0
+            
+            if pygame.key.get_pressed()[pygame.K_s] and self.attackCooldown == 0 and self.bulletsOut < 3:
+                self.attackCooldown = 10
+                self.bulletsOut += 1
 
-                    npcs.append(npcClass)
+                offset = 0
+                if self.direction == 1:
+                    offset = self.width
+                
+                sectionX = self.rect.x - sections[self.section].x
+                sectionY = self.rect.y - sections[self.section].y
+
+                npcClass = NPC(1, sectionX + offset, sectionY, self.section, self.direction)
+
+                npcs.append(npcClass)
+    
+    def update(self):
+        if not camera.isUpdating:
+            self.handleControls()
             
             if self.hasJumped and self.jumpTimer < 22:
                 self.speedY = -2.2
@@ -89,6 +123,11 @@ class Player():
 
             if self.attackCooldown > 0:
                 self.attackCooldown -= 1
+
+            if self.isClimbing:
+                self.width = 16
+            else:
+                self.width = 32
         else:
             if camera.moveDir == DIR_LEFT or camera.moveDir == DIR_RIGHT:
                 self.direction = camera.moveDir
@@ -97,86 +136,161 @@ class Player():
                 if self.isOnGround:
                     self.speedX = self.direction
 
-        for k, v in enumerate(getBlock(BLOCK_SOLID)):
+        for k, b in enumerate(blocks):
+            # ladder collision
+            if b.climbable:
+                if b.rect.colliderect(self.rect.x - 8, self.rect.y - 8, self.width + 8, self.height + 8):
+                    self.nearLadder = True
+                else:
+                    self.nearLadder = False
+                
+                if self.isClimbing:
+                    self.rect.x = b.rect.x
+            
             # horizontal collision
-            if v.rect.colliderect(self.rect.x + self.speedX, self.rect.y, self.width, self.height):
-                self.speedX = 0
+            if b.solidSide:
+                if b.rect.colliderect(self.rect.x + self.speedX, self.rect.y, self.width, self.height):
+                    self.speedX = 0
 
             # vertical collision
-            if v.rect.colliderect(self.rect.x, self.rect.y + self.speedY, self.width, self.height):
+            if b.rect.colliderect(self.rect.x, self.rect.y + self.speedY, self.width, self.height):
                 # below the block
                 if self.speedY < 0:
-                    self.speedY = v.rect.bottom - self.rect.top
-                    self.speedY = 0
+                    if b.solidBottom:
+                        self.speedY = b.rect.bottom - self.rect.top
+                        self.speedY = 0
                 # above the block
-                elif self.speedY >= 0:
-                    self.speedY = v.rect.top - self.rect.bottom
-                    # reset jump status
-                    self.hasJumped = False
-                    self.jumpTimer = 0
-                    self.isOnGround = True
+                elif self.speedY > 0:
+                    if b.solidTop:
+                        if not self.isClimbing:
+                            if b.solidSide:
+                                self.speedY = b.rect.top - self.rect.bottom
+                            else:
+                                if (player.rect.x + player.speedX >= b.rect.top):
+                                    self.speedY = b.rect.top - self.rect.bottom
+                        else:
+                            if b.solidSide:
+                                self.speedY = b.rect.top - self.rect.bottom
+                        # reset jump status
+                        self.hasJumped = False
+                        self.jumpTimer = 0
+                        self.isOnGround = True
+
+                        if b.climbable:
+                            self.nearLadder = True
+                
+            #dismount from ladders
+            if player.isClimbing:
+                if b.climbable and b.solidTop:
+                    if (self.rect.bottom < b.rect.top + 2):
+                        self.isClimbing = False
 
         self.rect.x += self.speedX
         self.rect.y += self.speedY
+
+        #check which section the player is in
+        for k, s in enumerate(sections):
+            if player.rect.x >= s.x and player.rect.x <= s.x + s.width:
+                player.section = k
 
         screen.blit(self.gfx, (self.rect.x - camera.x, self.rect.y - camera.y))
         #pygame.draw.rect(screen, (255, 0, 0), self.rect, 2)
 
 class Block():
-    def __init__(self, id: int, x: int, y: int) -> None:
-        self.id             = id
-        self.gfx            = pygame.image.load(f"pygame project\\block\\block-{id}.png")
-        self.rect           = self.gfx.get_rect()
-        self.width          = self.rect.width
-        self.height         = self.rect.height
+    def __init__(self, id: int, x: int, y: int, section: int) -> None:
+        self.id                 = id
+        self.gfx                = pygame.image.load(f"pygame project\\block\\block-{id}.png")
+        self.rect               = self.gfx.get_rect()
+        self.width              = self.rect.width
+        self.height             = self.rect.height
         # Starting location
-        self.rect.x         = x
-        self.rect.y         = y
+        self.rect.x             = sections[section].x + x
+        self.rect.y             = sections[section].y + y
+        # block behavior
+        self.solidSide          = False
+        self.solidBottom        = False
+        self.solidTop           = False
+        self.climbable          = False
+        self.foreground         = False
+
+        if self.id in BLOCK_CLIMBABLE:
+            self.climbable      = True
+        if self.id in BLOCK_SOLID:
+            self.solidSide      = True
+            self.solidBottom    = True
+            self.solidTop       = True
+            self.foreground     = True
+        if self.id in BLOCK_SEMISOLID:
+            self.solidTop       = True
     
     def update(self):
         screenX = self.rect.x - camera.x
         screenY = self.rect.y - camera.y
         
-        if screenX > -16 and screenX < 272 and screenY > -16 and screenY < 240:
+        if screenX > -16 and screenX < width + 16 and screenY > -16 and screenY < height + 16:
             screen.blit(self.gfx, (screenX, screenY))
         #pygame.draw.rect(screen, (0, 0, 255), self.rect, 2)
 
 class NPC():
-    def __init__(self, id: int, x: int, y: int, direction: int) -> None:
+    def __init__(self, id: int, x: int, y: int, section: int, direction: int) -> None:
         self.id             = id
         self.gfx            = pygame.image.load(f"pygame project\\npc\\npc-{id}.png")
         self.rect           = self.gfx.get_rect()
         self.width          = self.rect.width
         self.height         = self.rect.height
         # Starting location
-        self.rect.x         = x
-        self.rect.y         = y
+        self.section        = section
+        self.rect.x         = sections[section].x + x
+        self.rect.y         = sections[section].y + y
         # NPC vars
-        self.isValid        = True              #if it's alive
-        self.isActive       = True              #if it's on-screen
+        self.isValid        = True              # if it's alive
+        self.isActive       = True              # if it's on-screen
         self.direction      = direction
         self.speedX         = 0
         self.speedY         = 0
+        self.health         = 0                 # if the npc relies on health, it won't die until it reaches 0.
+        self.immuneFrames   = 0                 # invincibility frames (only when > 0)
+    
+    def kill(self):
+        self.isValid = False
+        self.isActive = False
+        
+        npcs.remove(self)
+    
+    def harm(self, damage: int, invincibleFrames: int):
+        self.health = self.health - damage
+
+        if self.health <= 0:
+            self.kill()
+        else:
+            self.immuneFrames = invincibleFrames
     
     def update(self):
+        # the actual npc behavior
         if self.isValid:
             if self.id == 1:
-                self.speedX = self.direction * 3
+                self.speedX = self.direction * 4
 
                 if self.rect.x + self.width < camera.x or self.rect.x > camera.x + camera.width:
                     player.bulletsOut -= 1
-                    npcs.remove(self)
+                    self.kill()
                 if camera.isUpdating:
                     player.bulletsOut -= 1
-                    npcs.remove(self)
+                    self.kill()
             
             self.rect.x += self.speedX
             self.rect.y += self.speedY
 
+        # render npcs
         screenX = self.rect.x - camera.x
         screenY = self.rect.y - camera.y
+
+        # check which section the npc is in
+        for k, s in enumerate(sections):
+            if self.rect.x >= s.x and self.rect.x <= s.x + s.width:
+                self.section = k
         
-        if screenX > -16 and screenX < 272 and screenY > -16 and screenY < 240:
+        if screenX > -16 and screenX < width + 16 and screenY > -16 and screenY < height + 16:
             screen.blit(self.gfx, (screenX, screenY))
             self.isActive = True
         else:
@@ -195,19 +309,20 @@ class Camera():
         self.targetY    = 0
         self.moveDir    = 0
         self.isUpdating = False
+        self.section    = player.section
     
     def update(self):
         if not self.isUpdating:
-            if player.rect.x + player.width > self.x + self.width: # right
+            if player.rect.x + player.width > sections[self.section].x + sections[self.section].width: # right
                 self.dx = self.width
                 self.targetX = self.x + self.width
                 self.moveDir = DIR_RIGHT
-            elif player.rect.x < self.x: # left
+            elif player.rect.x < sections[self.section].x: # left
                 self.dx = -self.width
                 self.targetX = self.x - self.width
                 self.moveDir = DIR_LEFT
             self.isUpdating = True
-        
+                
         if self.dx != 0:
             if self.dx > 0:
                 self.dx -= 4
@@ -226,41 +341,55 @@ class Camera():
         
         if (self.dx == 0) and (self.dy == 0):
             self.isUpdating = False
+        else:
+            self.section = player.section
 
 # gameplay objects
-player = Player(16, 160)
+sections = [
+    Section(0, 0, width * 2, height),
+    Section(-width, 0, width, height)
+]
+
+player = Player(16, 160, 0)
 camera = Camera(0, 0)
 clock = pygame.time.Clock()
 
 blocks = [
-    #Block(3, -48, 192),
-    Block(3, -32, 192),
-    Block(3, -16, 192),
-    Block(3, 0, 192),
-    Block(3, 16, 192),
-    Block(3, 32, 192),
-    Block(1, 48, 160),
-    Block(1, 48, 176),
-    Block(3, 48, 192),
-    Block(3, 64, 192),
-    Block(3, 80, 192),
-    Block(4, 96, 192),
-    Block(1, 64, 64),
-    Block(1, 80, 64),
-    Block(1, 96, 64),
-    Block(3, 112, 208),
-    Block(11, 112, 192),
-    Block(11, 112, 176),
-    Block(11, 112, 160),
-    Block(4, 128, 208),
-    Block(2, 208, 208),
-    Block(3, 224, 208),
-    Block(3, 240, 208),
-    Block(3, 256, 208),
-    Block(3, 272, 208),
-    Block(3, 288, 208),
-    Block(3, 304, 208),
-    Block(4, 320, 208)
+    #Block(3, 208, 192, 1),
+    Block(3, 224, 192, 1),
+    Block(3, 240, 192, 1),
+    Block(3, 0, 192, 0),
+    Block(3, 16, 192, 0),
+    Block(3, 32, 192, 0),
+    Block(1, 48, 160, 0),
+    Block(1, 48, 176, 0),
+    Block(3, 48, 192, 0),
+    Block(3, 64, 192, 0),
+    Block(3, 80, 192, 0),
+    Block(4, 96, 192, 0),
+    Block(1, 64, 64, 0),
+    Block(1, 80, 64, 0),
+    Block(1, 96, 64, 0),
+    Block(3, 112, 208, 0),
+    #Block(1, 128, 64, 0),
+    Block(12, 112, 64, 0),
+    Block(11, 112, 80, 0),
+    Block(11, 112, 96, 0),
+    Block(11, 112, 112, 0),
+    Block(11, 112, 128, 0),
+    Block(11, 112, 144, 0),
+    Block(11, 112, 160, 0),
+    Block(11, 112, 176, 0),
+    Block(11, 112, 192, 0),
+    Block(4, 128, 208, 0),
+    Block(2, 208, 208, 0),
+    Block(3, 224, 208, 0),
+    Block(3, 240, 208, 0),
+    Block(3, 256, 208, 0),
+    Block(3, 272, 208, 0),
+    Block(3, 288, 208, 0),
+    Block(3, 304, 208, 0),
+    Block(4, 320, 208, 0)
 ]
 
 npcs = []
@@ -271,11 +400,17 @@ while True:
     
     if inGame:
         screen.fill(black)
+
+        for k, b in enumerate(blocks):
+            if not b.foreground:
+                b.update()
         
         player.update()
 
+        # foreground blocks
         for k, b in enumerate(blocks):
-            b.update()
+            if b.foreground:
+                b.update()
         
         for k, v in enumerate(npcs):
             v.update()
--- test/test.py
+++ test/test.py
@@ -1,107 +1,24 @@
-sel = -1
-sel2 = 0
-sel3 = 0
-lang = 0
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-langTable = [
-    ["     SELECTION", "[1] Triangle", "[2] Rectangle", "[3] Square", "[4] Parallelogram", "[1] Area",
-     "[2] Perimeter", "Input the base of the triangle: ", "Input the height of the triangle: ",
-     "Input the first side of the triangle: ", "Input the second side of the triangle: ",
-     "Input the third side of the triangle: ", "Input the length of the rectangle: ",
-     "Input the width of the rectangle: ", "Input the length of the square: ",
-     "Input the base of the parallelogram: ", "Input the height of the parallelogram: ",
-     "Input the first side of the parallelogram: ", "Input the second side of the parallelogram: ",
-     "The area of the triangle is ", "The perimeter of the triangle is ", "The area of the rectangle is ",
-     "The perimeter of the rectangle is ", "The area of the square is ", "The perimeter of the square is ",
-     "The area of the parallelogram is ", "The perimeter of the parallelogram is ", " units."
-     ], #English
-    ["      Auswahl", "[1] Dreieck", "[2] Rechteck", "[3] Quadrat", "[4] Parallelogramm", 
-     "[1] Flächeninhalt", "[2] Umfang", "Geben Sie die Basis des Dreiecks ein: ",
-     "Geben Sie die Höhe des Dreiecks ein: ", "Geben Sie die erste Seite des Dreiecks ein: ",
-     "Geben Sie die zweite Seite des Dreiecks ein: ", "Geben Sie die dritte Seite des Dreiecks ein: ",
-     "Geben Sie die Länge des Rechtecks ein: ", "Geben Sie die Breite des Rechtecks ein: ",
-     "Geben Sie die Seitenlänge des Quadrats ein: ", "Geben Sie die Basis de Parallelogramms ein: ",
-     "Geben Sie die Höhe de Parallelogramms ein: ", "Geben Sie die erste Siete des Parallelogramms ein: ",
-     "Geben Sie die zweite Siete des Parallelogramms ein: ", "Die Flächeninhalt des Dreiecks beträgt ",
-     "Der Umfang des Dreiecks beträgt ", "Die Flächeninhalt des Rechtecks beträgt ",
-     "Der Umfang des Rechtecks beträgt ", "Die Flächeninhalt des Quadrats beträgt ",
-     "Der Umfang des Quadrats beträgt ", "Die Flächeninhalt des Parallelogramms beträgt ",
-     "Der Umfang des Parallelogramms beträgt ", " Einheiten."
-     ], #Deutsch
-    ["     SÉLECTION", "[1] Triangle", "[2] Rectangle", "[3] Carré", "[4] Parallélogramme", "[1] Aire",
-     "[2] Périmètre", "Saisissez la base du triangle : ", "Saisissez la hauteur du triangle : ",
-     "Saisissez le premier côté du triangle : ", "Saisissez le deuxième côté du triangle : ",
-     "Saisissez le troisième côté du triangle : ", "Saisissez la longueur du rectangle : ",
-     "Saisissez la largeur du rectangle : ", "Saisissez le côté du carré : ",
-     "Saisissez la base du parallélogramme : ", "Saisissez la hauteur du parallélogramme : ",
-     "Saisissez le premier côté du parallélogramme : ", "Saisissez le deuxième côté du parallélogramme : ",
-     "L'aire du triangle est de ", "Le périmètre du triangle est de ", "L'aire du rectangle est de ",
-     "Le périmètre du rectangle est de ", "L'aire du carré est de ", "Le périmètre du carré est de ",
-     "L'aire du parallélogramme est de ", "Le périmètre du parallélogramme est de ", " unités."
-    ], # Français
-    ["     SELECCIÓN", "[1] Triángulo", "[2] Rectángulo", "[3] Cuadrado", "[4] Paralelogramo", "[1] Área",
-     "[2] Perímetro", "Ingrese la base del triángulo: ", "Ingrese la altura del triángulo: ",
-     "Ingrese el primer lado del triángulo: ", "Ingrese el segundo lado del triángulo: ",
-     "Ingrese el tercer lado del triángulo: ", "Ingrese la longitud del rectángulo: ",
-     "Ingrese el ancho del rectángulo: ", "Ingrese el lado del cuadrado: ",
-     "Ingrese la base del paralelogramo: ", "Ingrese la altura del paralelogramo: ",
-     "Ingrese el primer lado del paralelogramo: ", "Ingrese el segundo lado del paralelogramo: ",
-     "El área del triángulo es ", "El perímetro del triángulo es ", "El área del rectángulo es ",
-     "El perímetro del rectángulo es ", "El área del paralelogramo es ", "El perímetro del paralelogramo es ",
-     " unidades."
-    ], # Español
-    ["     SELEZIONE", "[1] Triangolo", "[2] Rettangolo", "[3] Quadrato", "[4] Parallelogramma", "[1] Area",
-     "[2] Perimetro", "Inserisci la base del triangolo: ", "Inserisci l'altezza del triangolo: ",
-     "Inserisci il primo lato del triangolo: ", "Inserisci il secondo lato del triangolo: ",
-     "Inserisci il terzo lato del triangolo: ", "Inserisci la lunghezza del rettangolo: ",
-     "Inserisci la larghezza del rettangolo: ", "Inserisci la lato del quadrato: ",
-     "Inserisci la base del parallelogramma: ", "Inserisci l'altezza del parallelogramma: ",
-     "Inserisci il primo lato del parallelogramma: ", "Inserisci il secondo lato del parallelogramma: ",
-     "L'area del triangolo è ", "Il perimetro del triangolo è ", "L'area del rettangolo è ",
-     "Il perimetro del rettangolo è ", "L'area del quadrato è ", "Il perimetro del quadrato è ",
-     "L'area del parallelogramma è ", "Il perimetro del parallelogramma è ", " unità."
-    ], # Italiano
-]
+PRIME_NUMS = [2, 3, 5, 7]
 
-print("LANGUAGE SELECT")
-print("====================")
-print("[1] English")
-print("[2] Deutsch")
-print("[3] Français")
-print("[4] Español")
-print("[5] Italiano")
-print("[6] 日本語")
-print("[7] Bahasa Indonesia")
-lang = int(input())
+print("a = [", end = "")
+for i in a:
+    print(i, end = "")
 
-while True:
-    print(langTable[lang - 1][0])
-    print("===================")
-    print(langTable[lang - 1][1])
-    print(langTable[lang - 1][2])
-    print(langTable[lang - 1][3])
-    print(langTable[lang - 1][4])
-    sel = int(input())
-    
-    if sel != 0:
-        print("===================")
-        print(langTable[lang - 1][5])
-        print(langTable[lang - 1][6])
-        sel2 = int(input())
-    else:
-        break
+    if i != len(a):
+        print(", ", end = "")
+print("]")
 
-    if sel == 1:
-        if sel2 == 1:
-            a = int(input(langTable[lang - 1][7]))
-            t = int(input(langTable[lang - 1][8]))
+b = a
+for i in b:
+    if i in PRIME_NUMS:
+        b.remove(i)
 
-            area = 0.5 * a * t
-            print(f"{langTable[lang - 1][19]}{area}{langTable[lang - 1][27]}")
-        else:
-            a = int(input(langTable[lang - 1][9]))
-            b = int(input(langTable[lang - 1][10]))
-            c = int(input(langTable[lang - 1][11]))
+print("b = [", end = "")
+for i in b:
+    print(i, end = "")
 
-            perimeter = a + b + c
-            print(f"{langTable[lang - 1][20]}{perimeter}{langTable[lang - 1][27]}")
+    if i != len(b):
+        print(", ", end = "")
+print("]")
