968f33ef1e81187a74ada6432fc554216958e895
Actually IAmPlayer <iamplayergamingyt@gmail.com>
2023-05-21 21:20:22 +0700

    merge sort program

--- test/merge_sort.py
+++ test/merge_sort.py
@@ -8,7 +8,10 @@ def mergeSort(arr):
         mergeSort(arrayL)
         mergeSort(arrayR)
 
-        i = j = k = 0
+        # initialize array indicies
+        i = 0
+        j = 0
+        k = 0
 
         # combining the halves back
         while i < len(arrayL) and j < len(arrayR):
@@ -31,7 +34,37 @@ def mergeSort(arr):
             j += 1
             k += 1
 
-exampleArray = [6, 5, 3, 1, 8, 7, 2, 4]
-print(f"Initial array: {exampleArray}")
-mergeSort(exampleArray)
-print(f"Sorted array: {exampleArray}")
+toSort = []
+
+def getArrayLength():
+    try:
+        n = int(input("Input the amount of entries in the array to sort with Merge Sort: "))
+    except ValueError:
+        print("Value is not a valid integer.")
+        getArrayLength()
+    else:
+        if n > 1:
+            getArrayValues(n)
+        else:
+            print("Value is not applicable for sorting.")
+            getArrayLength()
+
+def getArrayValues(arrayLength: int):
+    inserted = 0
+    while inserted < arrayLength:
+        try:
+            value = int(input(f"Input a number ({inserted + 1}/{arrayLength}): "))
+            toSort.append(value)
+        except ValueError:
+            print("Value is not a valid integer.")
+        else:
+            inserted += 1
+
+def main():
+    getArrayLength()
+    print(f"Before sorting: {toSort}")
+    mergeSort(toSort)
+    print(f"After sorting: {toSort}")
+
+if __name__ == "__main__":
+    main()
